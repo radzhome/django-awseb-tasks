947af1c47784256972d39f86ecc953bcae551e2f
Radomir Wojcik <radzhome@gmail.com>
2015-01-05 00:13:30 -0500

    added tests WIP

---
+++ aws_tasks/tests.py
@@ -0,0 +1,8 @@
+# import json
+# import os
+# import uuid
+#
+# from django.conf import settings
+# from django.core import mail
+# from django.core.urlresolvers import reverse
+# from django.test import TestCase
