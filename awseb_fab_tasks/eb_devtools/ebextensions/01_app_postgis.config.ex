6da875f147dd01859578be84351bec3204251fa8
Rad <radzhome@gmail.com>
2015-02-06 17:55:04 -0500

    fix for syncdb

--- awseb_fab_tasks/eb_devtools/ebextensions/01_app_postgis.config.ex
+++ awseb_fab_tasks/eb_devtools/ebextensions/01_app_postgis.config.ex
@@ -18,6 +18,9 @@ container_commands:
   01_collectstatic:
     command: 'PYTHONPATH=.:..:../lib cd site/{project} && ./manage.py collectstatic -c --noinput && cd ../..'
     leader_only: true
+  02_syncdb:
+    command: 'PYTHONPATH=.:..:../lib cd site/{project} && ./manage.py syncdb --noinput && cd ../..'
+    leader_only: true
   02_migrate:
     command: 'PYTHONPATH=.:..:../lib cd site/{project} && ./manage.py migrate --noinput && cd ../..'
     leader_only: true
